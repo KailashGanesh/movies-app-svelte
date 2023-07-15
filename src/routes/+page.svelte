bba28864bbc126ee26a6b89670dca909b0fbb909
KailashGanesh <58307685+KailashGanesh@users.noreply.github.com>
2023-07-15 16:52:21 +0530

    feat: made the mvoies app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,70 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-	let api_key = import.meta.env.VITE_API_KEY;
-	console.log(api_key);
+<script lang="ts">
+	import { onMount } from "svelte";
+	import Card from "./Card.svelte";
+
+	const API_KEY = import.meta.env.VITE_API_KEY;
+	const BASEURL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+	const SEARCHURL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&include_adult=false&query=`
+	const IMGPATH = 'https://image.tmdb.org/t/p/w500'
+
+	type moviesObject = {
+		adult: boolean,
+		backdrop_path: string,
+		genre_ids: number[],
+		id: number,
+		original_language: string,
+		original_title: string,
+		overview: string,
+		popularity: number,
+		poster_path: string,
+		release_date: string,
+		title: string,
+		video: boolean,
+		vote_average: number,
+		vote_count: number
+	}
+
+	let movies: moviesObject[] = [];
+	$: console.log(movies);
+
+	async function fetchData(): Promise<void> {
+		const response = await fetch(BASEURL);
+		const data = await response.json();
+		movies = data.results;
+	}
+
+	onMount(fetchData)
 </script>
 
 <svelte:head>
 	<title>Movies App in Sveltekit</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="a movies app in svltekit" />
 </svelte:head>
 
 <section>
-	<header></header>
-	<main>
-
-	</main>
-	<footer></footer>
+	<ul>
+		{ #each movies as movie (movie.id) }
+			<Card src={IMGPATH + movie.poster_path} alt={movie.title} title={movie.title} desc={movie.overview} />
+		{/each}
+	</ul>
 </section>
 
 <style>
+	ul {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: flex-start;
+		align-items: center;
+		padding: 0;
+		margin: 0;
+		list-style: none;
+		gap: 20px;
+	}
 	section {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
 		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
---
+++ src/routes/Card.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    export let src: string;
+    export let alt: string;
+    export let title: string;
+    export let desc: string;
+</script>
+
+<li>
+    <img src={src} alt={alt} />
+    <h2>{title}</h2>
+    <p>{desc}</p>
+</li>
+
+<style>
+    li {
+        list-style: none;
+        padding: 40px;
+        border: 1px solid #eee;
+        width: 200px;
+        box-shadow: gray 0px 0px 10px;
+        position: relative;
+        min-height: 350px;
+        max-height: 350px;
+    }
+    
+    li img{
+        max-width: 100%;
+    }
+
+    li p {
+        visibility: hidden;
+        overflow: hidden;
+        transition: all 1s ease-in-out;
+        position: absolute;
+        padding: 20px;
+        left: 0;
+        top: 25%;
+        color: transparent;
+    }
+
+    li:hover p {
+        visibility: visible;
+        background-color: #eee;
+        top: 0;
+        color: black;
+    }
+
+</style>
