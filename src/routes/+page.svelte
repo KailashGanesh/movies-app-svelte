4ed81bfb6dc95b59d446ee5c546a9fb401689745
KailashGanesh <58307685+KailashGanesh@users.noreply.github.com>
2023-07-16 18:56:44 +0530

    feat: pages works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { onMount } from "svelte";
 	import Card from "./Card.svelte";
-	import { base } from "$app/paths";
 
 	type moviesObject = {
 		adult: boolean,
@@ -25,16 +24,16 @@
 	let currentPage: number = 1;
 	let totalPages: number = 0;
 
-	let BASEURL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${currentPage}`
-
-	let movies: moviesObject[] = [];
-	$: console.log(movies);
-
-	const SEARCHURL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&include_adult=false&query=`
 	const IMGPATH = 'https://image.tmdb.org/t/p/w500'
 
+	let movies: moviesObject[] = [];
 
-
+	function constructUrl(searchTerm: string = ''): string {
+		if (searchTerm) {
+			return `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&include_adult=false&query=${searchTerm}&page=${currentPage}`;
+		}
+		return `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${currentPage}`;
+	}
 
 	async function fetchData(url: string): Promise<void> {
 		const response = await fetch(url);
@@ -43,27 +42,25 @@
 		totalPages = data.total_pages;
 	}
 
-
 	function handlePageChange(nextORPrev: "prev" | "next"): void {
 		switch (nextORPrev) {
 			case "prev":
 				if (currentPage > 1) {
 					currentPage = currentPage - 1;
-					BASEURL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${currentPage}`
 				}	
 				break;
 			case "next":
 				if (currentPage < totalPages) {
 					currentPage = currentPage + 1;
-					BASEURL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${currentPage}`
 				}
 				break;
 			default:
 				break;
 		}
+		fetchData(constructUrl()); // fetch new page data here
 	}
 
-	onMount(() => fetchData(BASEURL));
+	onMount(() => fetchData(constructUrl()));
 </script>
 
 <svelte:head>
@@ -71,6 +68,13 @@
 	<meta name="description" content="a movies app in svltekit" />
 </svelte:head>
 
+<section class="page">
+	<p>{currentPage} of {totalPages}</p>
+	<div>
+		<button on:click={() => handlePageChange("prev")}>{"<"}</button>
+		<button on:click={() => handlePageChange("next")}>{">"}</button>
+	</div>
+</section>
 <section>
 	<ul>
 		{ #each movies as movie (movie.id) }
@@ -79,9 +83,11 @@
 	</ul>
 </section>
 <section class="page">
-	<span>{currentPage} of {totalPages}</span>
-	<button on:click={() => handlePageChange("prev")}>{"<"}</button>
-	<button on:click={() => handlePageChange("next")}>{">"}</button>
+	<p>{currentPage} of {totalPages}</p>
+	<div>
+		<button on:click={() => handlePageChange("prev")}>{"<"}</button>
+		<button on:click={() => handlePageChange("next")}>{">"}</button>
+	</div>
 </section>
 
 <style>
@@ -101,5 +107,12 @@
 		justify-content: center;
 		align-items: center;
 		flex: 0.6;
+	}
+	.page {
+		margin-block: 20px;
+	}
+	.page div {
+		display: flex;
+		max-width: 500px;
 	}
 </style>
